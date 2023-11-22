abd817f58d361524069d4544ecac6be3c0ea741a
Your Name <vincegaurino@gmail.com>
2023-11-22 17:38:29 +0800

    optimize

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>ChaTangina</h1>
+        <h1>ChaTangina💩</h1>
         {user ? <button onClick={() => auth.signOut()}>Sign Out</button> : null}
       </header>
 
